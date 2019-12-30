d49fe36863ab5524a8dff475e8e5e85cae824d0f
Chito-XD <chito99salu@outlook.com>
2019-12-29 21:06:02 -0600

    creating components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './comonents/navigation/navigation.component';
+import { GameFormComponent } from './comonents/game-form/game-form.component';
+import { GameListComponent } from './comonents/game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    GameFormComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
