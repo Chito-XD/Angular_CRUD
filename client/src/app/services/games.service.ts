e29d062c6fbf768a6a7010814ca830a3760e1051
Chito-XD <chito99salu@outlook.com>
2019-12-29 22:39:45 -0600

    fix

--- client/src/app/services/games.service.ts
+++ client/src/app/services/games.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Game } from '../models/Game';
+import { Game } from '../models/Game.model';
 
 @Injectable({
   providedIn: 'root'
